88654c164e6e18d654ec1e55f7b637157ebf3320
M1core <44338439+M1core@users.noreply.github.com>
2020-07-07 18:11:07 +0300

    added auth

--- src/index.js
+++ src/index.js
@@ -13,36 +13,44 @@ app.listen(process.env.PORT || 3000, () => console.log('Server working at port:'
 appCache.mset([
   { key: 'executions', val: 0 },
   { key: 'executionTimers', val: [] },
+  { key: 'isAuth', val: false },
 ]);
 
+app.post('/api/auth', (req, res) => {
+  appCache.set('isAuth', true);
+  res.send('Succesfully authorized');
+});
+
 app.post('/api/comments', (req, res) => {
-  if (appCache.get('executions') < 5) {
-    appCache.set('executions', appCache.get('executions') + 1);
+  if (appCache.get('isAuth')) {
+    if (appCache.get('executions') < 5) {
+      appCache.set('executions', appCache.get('executions') + 1);
 
-    const timerStart = new Date().getTime();
+      const timerStart = new Date().getTime();
 
-    getComments().then((data) => {
-      const user = topUser(data);
-      const words = Object.fromEntries(topWords(data));
+      getComments().then((data) => {
+        const user = topUser(data);
+        const words = Object.fromEntries(topWords(data));
 
-      const timerEnd = new Date().getTime() - timerStart;
-      const timerArr = appCache.get('executionTimers');
-      timerArr.length > 3 ? () => { timerArr.unshift(); timerArr.push(timerEnd); }
-        : timerArr.push(timerEnd);
-      appCache.set('executionTimers', timerArr);
+        const timerEnd = new Date().getTime() - timerStart;
+        const timerArr = appCache.get('executionTimers');
+        timerArr.length > 3 ? () => { timerArr.unshift(); timerArr.push(timerEnd); }
+          : timerArr.push(timerEnd);
+        appCache.set('executionTimers', timerArr);
 
-      res.send({
-        popularAuthor: {
-          email: user[0],
-          comments: user[1],
-        },
-        popularWords: words,
-        executionTime: timerEnd,
-        avgExecTime: appCache.get('executionTimers').reduce((acc, cur) => acc + cur) / (appCache.get('executionTimers')).length,
+        res.send({
+          popularAuthor: {
+            email: user[0],
+            comments: user[1],
+          },
+          popularWords: words,
+          executionTime: timerEnd,
+          avgExecTime: appCache.get('executionTimers').reduce((acc, cur) => acc + cur) / (appCache.get('executionTimers')).length,
+        });
       });
-    });
-  } else {
-    res.send('You have made too many requests, please try again later');
-    setTimeout(() => appCache.set('executions', 0), 10000);
-  }
+    } else {
+      res.send('You have made too many requests, please try again later');
+      setTimeout(() => appCache.set('executions', 0), 10000);
+    }
+  } else res.send('You are not authorized. Visit "/api/auth".');
 });
